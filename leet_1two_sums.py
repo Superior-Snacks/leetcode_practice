b12d582b52deb67f707927bf8b1b0b1805d49e33
Superior-Snacks <joningiwork@gmail.com>
2024-11-06 18:33:17 +0000

    more test

--- leet_1two_sums.py
+++ leet_1two_sums.py
@@ -12,3 +12,15 @@ class Solution(object):
                 return [track[check], current]
             
 print("yo")
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    track = {}
+    for current, number in enumerate(nums):
+        check = target - number
+        if check in track:
+            return [track[check], current]
+print(twoSum([2,5,7,10], 9))
