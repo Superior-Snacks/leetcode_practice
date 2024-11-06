21a8cbec305a8b3c7aee7e897f039c3a49331ef9
Superior-Snacks <joningiwork@gmail.com>
2024-11-06 18:37:43 +0000

    small fix

--- leet_1two_sums.py
+++ leet_1two_sums.py
@@ -1,16 +1,3 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        track = {}
-        for current, number in enumerate(nums):
-            check = target - number
-            if check in track:
-                return [track[check], current]
-            
 def twoSum(nums, target):
     """
     :type nums: List[int]
