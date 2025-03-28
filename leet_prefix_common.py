59b4344553a33f6f638eee2d3837114f7bf933d7
Superior-Snacks <joningiwork@gmail.com>
2025-03-28 17:31:08 +0000

    s

---
+++ leet_prefix_common.py
@@ -0,0 +1,14 @@
+def run():
+    part = 0
+    while True:
+        single = set()
+        for i in strs:
+            single.add(i[part])
+        if len(single) != part + 1:
+            if part < 0:
+                return strs[0][:part]
+            else:
+                return ""
+        part += 1
+
+run()
