72890df0bde00908ad2103160312831a7d188b29
Superior-Snacks <joningiwork@gmail.com>
2025-03-28 15:04:17 +0000

    s

--- leet_13roman_to_intiger.py
+++ leet_13roman_to_intiger.py
@@ -36,16 +36,16 @@ Input: s = "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
-running = 0
-temp = 0
-rom = {"I" : 1,
-"V" : 5,
-"X" : 10,
-"L" : 50,
-"C" : 100,
-"D" : 500,
-"M" : 1000}
 def romanToInt(s):
+    running = 0
+    temp = 0
+    rom = {"I" : 1,
+    "V" : 5,
+    "X" : 10,
+    "L" : 50,
+    "C" : 100,
+    "D" : 500,
+    "M" : 1000}
     for i in s[::-1]:
         print(rom[i])
         if rom[i] < temp:
