ff1263492e2c00257fe74a90613bbd6e328e7422
Superior-Snacks <joningiwork@gmail.com>
2025-03-29 19:27:07 +0000

    s

--- leet_valid_bracket.py
+++ leet_valid_bracket.py
@@ -1,7 +1,7 @@
 def isValid(s):
     r = ["(", "[", "{"]
-    l = {")":"(", "]":"[", "}":"{"}
-    m = [")", "]", "}"]
+    m = {")":"(", "]":"[", "}":"{"}
+    l = [")", "]", "}"]
     # find first right side
     #go back, remove checked
     while True:
@@ -9,28 +9,28 @@ def isValid(s):
         if s:
             for i in range(len(s)):
                 if s[i] in l: #find right
-                    k = l[s[i]]
+                    k = s[i]
                     print(k)
                     j = r[i-1]
                     print(j)
                     print("#find right")
-                    if k == j: # check if close
-                        s = s[:j] + s[k:] #remove valid and conitnue
+                    if m[k] == j: # check if close
+                        s = s[:i-1] + s[i:] #remove valid and conitnue
                         print("#remove valid and conitnue")
                         print(f"string is {s}")
                         break
                     else: # if not valid string bad
-                        False
+                        return False
                         print("if not valid string bad")
                 else:
                     print("na not it")
         else: #if no left means valid string
             print("#if no left means valid string")
-            True
-print(isValid("()"))
-print(isValid("()[]{}"))
-print(isValid("(]"))
-print(isValid("([])"))
+            return True
+print("result ", isValid("()"))
+print("result ", isValid("()[]{}"))
+print("result ", isValid("(]"))
+print("result ", isValid("([])"))
 
 
 """Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
