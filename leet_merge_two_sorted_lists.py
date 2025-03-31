df20b05406ae783547e9ee1a7b36156d774e2c7a
Superior-Snacks <joningiwork@gmail.com>
2025-03-31 16:21:51 +0000

    s

--- leet_merge_two_sorted_lists.py
+++ leet_merge_two_sorted_lists.py
@@ -9,18 +9,18 @@ def mergeTwoLists(list1, list2):
     remainder1 = []
     for i in range(len(list1)):
         if list1[i] < list2[i]:
-            output.append(remainder1)
-            output.append(list1[i])
-            remainder2.append(list2[i])
+            output.extend(remainder1)
+            output.extend(list1[i])
+            remainder2.extend(list2[i])
             remainder1 = []
         elif list2[i] < list1[i]:
-            output.append(remainder2)
-            output.append(list2[i])
-            remainder1.append(list1[i])
+            output.extend(remainder2)
+            output.extend(list2[i])
+            remainder1.extend(list1[i])
             remainder2 = []
         else:
-            output.append(remainder1)
-            output.append(remainder2)
+            output.extend(remainder1)
+            output.extend(remainder2)
             remainder1 = []
             remainder2 = []
     return output
