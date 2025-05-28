0bef372dcd4ffbc8cfc86ed069c9a2a53eb0d41b
Superior-Snacks <joningiwork@gmail.com>
2025-05-28 19:25:10 +0000

    s

--- leet_27_remove_elements.py
+++ leet_27_remove_elements.py
@@ -10,7 +10,7 @@ def removeElement(nums, val):
         if nums[search] == val:
             nums[search], nums[point] = nums[point], nums[search]
             search += 1
-    print(nums)
+    print(nums, point)
 
 
 
