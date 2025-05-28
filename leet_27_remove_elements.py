b751e829d8c159eb394aca52bcf39ac0b84b158f
Superior-Snacks <joningiwork@gmail.com>
2025-05-28 17:04:12 +0000

    s

--- leet_27_remove_elements.py
+++ leet_27_remove_elements.py
@@ -5,3 +5,7 @@ def removeElement(nums, val):
 
 
 removeElement([3,2,2,3], 3)
+removeElement([3,4,8,8], 8)
+removeElement([1,2,3,4,5,6,6,4], 6)
+removeElement([1,1,1,2,2,2,4,4,1], 1)
+removeElement([1,1,1,1], 3)
