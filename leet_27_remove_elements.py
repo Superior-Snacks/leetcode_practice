7ef1ca1ad2e39e6524dadd932ac063122903723e
Superior-Snacks <joningiwork@gmail.com>
2025-05-28 19:43:39 +0000

    s

--- leet_27_remove_elements.py
+++ leet_27_remove_elements.py
@@ -22,3 +22,12 @@ print(removeElement([1,2,3,4,5,6,6,4], 6))
 print(removeElement([1,1,1,2,2,2,4,4,1], 1))
 print(removeElement([1,1,1,1], 3))
 print(removeElement([3,3], 3))
+print(removeElement([2], 2))
+print(removeElement([], 0))
+print(removeElement([1,2,3,4], 5))
+
+[2], val = 2
+
+[], val = 0
+
+[1,2,3,4], val = 5
