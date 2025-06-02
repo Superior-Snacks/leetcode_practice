e58d6695fb3dfb84c1cbff9c3abf2bd15481c8e4
Superior-Snacks <joningiwork@gmail.com>
2025-06-02 19:05:10 +0000

    s

---
+++ leet_35_search_insert.py
@@ -0,0 +1,13 @@
+def searchInsert( nums, target):
+    place = 0
+    current = None
+    while current < target:
+        current = nums[place]
+        if current == target:
+            return place
+        elif current > target:
+            return place
+        else:
+            place += 1
+
+print(searchInsert([1,2,3,4,5,6], 5))
