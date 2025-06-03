69810be3992e8e4ac2ddc1471582dd8d8954dcde
Superior-Snacks <joningiwork@gmail.com>
2025-06-03 15:43:51 +0000

    s

--- leet_58_length_of_last.py
+++ leet_58_length_of_last.py
@@ -5,6 +5,8 @@ def lengthOfLastWord(s):
         print(s[i])
         if allowed(s[i]):
             count += 1
+        elif not allowed(s[i]) and count == 0:
+
 
 def allowed(x):
     if x in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
