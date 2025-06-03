8631def4d9a026226cd68520d712a4e87027d4d1
Superior-Snacks <joningiwork@gmail.com>
2025-06-03 15:36:26 +0000

    s

--- leet_58_length_of_last.py
+++ leet_58_length_of_last.py
@@ -1,6 +1,6 @@
 def lengthOfLastWord(s):
     print(s)
-    for i in range(len(s)):
+    for i in s:
         print(i)
 
 
@@ -13,6 +13,7 @@ print(lengthOfLastWord("solo     "))  # Expected: 4
 print(lengthOfLastWord("yes"))  # Expected: 3
 print(lengthOfLastWord("word before    final"))  # Expected: 5
 print(lengthOfLastWord("           word"))  # Expected: 4
-print(lengthOfLastWord("a " * 9999 + "end"))  # Expected: 3
+s = "a " * 9999 + "end"
+print(lengthOfLastWord(s))  # Expected: 3
 print(lengthOfLastWord("     "))  # Behavior undefined per constraints, should never occur.
 print(lengthOfLastWord("MiXeDCaSe"))  # Expected: 9
