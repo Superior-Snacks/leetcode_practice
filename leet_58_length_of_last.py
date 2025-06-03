37d4a1ed5c7b98808cbc9be79a3c4c1c9840dd5e
Superior-Snacks <joningiwork@gmail.com>
2025-06-03 15:27:44 +0000

    s

--- leet_58_length_of_last.py
+++ leet_58_length_of_last.py
@@ -4,3 +4,12 @@ def lengthOfLastWord(self, s):
 
 
 print(lengthOfLastWord("Hello World"))  # Expected: 5
+print(lengthOfLastWord("   fly me   to   the moon  "))  # Expected: 4
+print(lengthOfLastWord("Hello"))  # Expected: 5
+print(lengthOfLastWord("world        "))  # Expected: 5
+print(lengthOfLastWord("a   b    c   d   e   "))  # Expected: 1
+print(lengthOfLastWord("solo     "))  # Expected: 4
+print(lengthOfLastWord("yes"))  # Expected: 3
+print(lengthOfLastWord("word before    final"))  # Expected: 5
+print(lengthOfLastWord("           word"))  # Expected: 4
+print(lengthOfLastWord("a " * 9999 + "end"))  # Expected: 3
