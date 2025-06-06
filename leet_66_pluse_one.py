2938219d77b00c76e4b85eac5767e00e2a9b83a0
Superior-Snacks <joningiwork@gmail.com>
2025-06-06 19:33:35 +0000

    s

--- leet_66_pluse_one.py
+++ leet_66_pluse_one.py
@@ -11,4 +11,15 @@ def plusOne(digits):
     return digits
 
 
-print(plusOne([8,9,9,9]))
+print(plusOne([1, 2, 3]))     # [1, 2, 4]
+print(plusOne([4, 3, 2, 1]))  # [4, 3, 2, 2]
+print(plusOne([9]))           # [1, 0]
+print(plusOne([1, 2, 9]))     # [1, 3, 0]
+print(plusOne([9, 9]))        # [1, 0, 0]
+print(plusOne([2, 9, 9]))     # [3, 0, 0]
+print(plusOne([9, 9, 9]))     # [1, 0, 0, 0]
+print(plusOne([9]*10))       # [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+print(plusOne([0]))           # [1]
+print(plusOne([1]))           # [2]
+print(plusOne([8]))           # [9]
+print(plusOne([1]*99 + [2]))  # ends in [1]*99 + [3]
