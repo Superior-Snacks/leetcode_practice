84a57f2c92e5fa331626b4f9e05f7960680bece6
Superior-Snacks <joningiwork@gmail.com>
2025-06-06 19:32:22 +0000

    s

--- leet_66_pluse_one.py
+++ leet_66_pluse_one.py
@@ -1,4 +1,5 @@
 def plusOne(digits):
+    overflow = [1]
     for i in range(len(digits) -1, -1, -1):
         if digits[i] == 9:
             digits[i] = 0
@@ -6,7 +7,8 @@ def plusOne(digits):
         elif digits[i] != 9:
             digits[i] += 1
             return digits
+    digits.insert(0,1)
+    return digits
 
 
-
-print(plusOne([9,9,9,9]))
+print(plusOne([8,9,9,9]))
