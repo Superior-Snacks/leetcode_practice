a64443bab46056ab80b6a298677111518e3741f9
Superior-Snacks <joningiwork@gmail.com>
2025-06-09 20:51:26 +0000

    s

--- leet_67_add_binary.py
+++ leet_67_add_binary.py
@@ -2,6 +2,7 @@ def addBinary(a, b):
     count = 0
     i = len(a) - 1
     j = len(b) - 1
+    
 
 
 
@@ -23,22 +24,16 @@ print(addBinary("0001", "0010"), "=> 11")
 """
 Given two binary strings a and b, return their sum as a binary string.
 
- 
-
 Example 1:
-
 Input: a = "11", b = "1"
 Output: "100"
-Example 2:
 
+Example 2:
 Input: a = "1010", b = "1011"
 Output: "10101"
  
-
 Constraints:
-
 1 <= a.length, b.length <= 104
 a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
-
 """
