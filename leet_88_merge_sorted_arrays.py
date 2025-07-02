0a65cf8b59e4930caf01d300810b8e9147ef53a4
Superior-Snacks <joningiwork@gmail.com>
2025-07-02 17:01:45 +0000

    s

--- leet_88_merge_sorted_arrays.py
+++ leet_88_merge_sorted_arrays.py
@@ -1,12 +1,5 @@
 def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
-        
+    ...
 
 # Example 1
 nums1 = [1,2,3,0,0,0]
