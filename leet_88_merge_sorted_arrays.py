66f74dc2e54efac8538a83be04f2664df52fbe62
Superior-Snacks <joningiwork@gmail.com>
2025-07-04 19:05:37 +0000

    s

--- leet_88_merge_sorted_arrays.py
+++ leet_88_merge_sorted_arrays.py
@@ -9,11 +9,11 @@ def merge(nums1, m, nums2, n):
         l1 = m -1
         l2 = -1
         for i in range(m + n - 1, -1, -1):
-            if l2 > 0:
-                break
-            if l1 > 0:
+            if l1 < 0:
                 nums1[i] = nums2[l2]
                 l2 -= 1
+            if l2 > 0:
+                break
             print(nums1[l1])
             print(nums2[l2])
             if nums2[l2] > nums1[l1]:
