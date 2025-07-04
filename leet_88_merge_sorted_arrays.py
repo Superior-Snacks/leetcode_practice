90a371b4f6bef7ba1c85726ee0c78cde1393eb56
Superior-Snacks <joningiwork@gmail.com>
2025-07-04 18:59:18 +0000

    s

--- leet_88_merge_sorted_arrays.py
+++ leet_88_merge_sorted_arrays.py
@@ -30,7 +30,7 @@ nums1 = [1,2,3,0,0,0]
 m = 3
 nums2 = [2,5,6]
 n = 3
-merge(nums1, m, nums2, n)
+#merge(nums1, m, nums2, n)
 print(f"input: [1,2,3,0,0,0], [2,5,6] =>{nums1}, expected: [1,2,2,3,5,6]")
 
 # Example 2
