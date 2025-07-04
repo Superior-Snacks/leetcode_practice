ecf1c37fc480cb98741ba1ea00d0c424f59ca273
Superior-Snacks <joningiwork@gmail.com>
2025-07-04 19:18:31 +0000

    s

--- leet_94_binary_indoor_travel.py
+++ leet_94_binary_indoor_travel.py
@@ -1,3 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+
+
+
+
+
+
+
+
+
 """
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
