eb3967aa74509ae6e76ac6697a84ccfe7466e1d2
Superior-Snacks <joningiwork@gmail.com>
2025-07-07 10:09:45 +0000

    s

--- leet_94_binary_indoor_travel.py
+++ leet_94_binary_indoor_travel.py
@@ -51,6 +51,7 @@ Input: root = []
 
 Output: []
 
+
 Example 4:
 
 Input: root = [1]
@@ -58,12 +59,9 @@ Input: root = [1]
 Output: [1]
 
  
-
 Constraints:
 
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
- 
-
 Follow up: Recursive solution is trivial, could you do it iteratively?
 """
