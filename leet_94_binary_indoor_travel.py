7924df9bbd967cc161def436f67f6d23e94ea0bd
Superior-Snacks <joningiwork@gmail.com>
2025-07-09 21:23:28 +0000

    s

--- leet_94_binary_indoor_travel.py
+++ leet_94_binary_indoor_travel.py
@@ -28,9 +28,6 @@ print("Case 7:", inorderTraversal([1, 2, None, 3]), "Expected: [3, 2, 1]")
 
 """
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
-
- 
-
 Example 1:
 
 Input: root = [1,null,2,3]
