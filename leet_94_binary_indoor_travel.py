2f4db801b8e595c05e50f146e8fee466f97943d3
Superior-Snacks <joningiwork@gmail.com>
2025-07-22 17:57:10 +0000

    s

--- leet_94_binary_indoor_travel.py
+++ leet_94_binary_indoor_travel.py
@@ -20,7 +20,7 @@ def inorderTraversal(root):
     #Right child index: 2 * i + 2
     while count < 10:
         for i in range(1, length + 1):
-            print(i)
+            print(root[2 * i + 1])
         count += 1
 
 # Example 1
