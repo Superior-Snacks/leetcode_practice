9a7a1dfece796aa394625ec294e2112ef5271528
Superior-Snacks <joningiwork@gmail.com>
2025-07-25 19:15:54 +0000

    s

--- leet_104_max_depth_bi_tree.py
+++ leet_104_max_depth_bi_tree.py
@@ -33,6 +33,7 @@ class Solution(object):
             return 0
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
+        return max(left_depth, right_depth) + 1
         
 
 
