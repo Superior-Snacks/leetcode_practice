d2b4d01dadf12a1783752dc2ab53fbfa353389e8
Superior-Snacks <joningiwork@gmail.com>
2025-07-26 18:43:33 +0000

    s

--- leet_108_convert_arr_to_tree.py
+++ leet_108_convert_arr_to_tree.py
@@ -10,4 +10,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: Optional[TreeNode]
         """
-        
+
+
+sol = Solution
+print(sol.sortedArrayToBST([-10,-3,0,5,9]))       # Mid is 0, expected root = 0
+print(sol.sortedArrayToBST([1,3]))               # Small even-size input
+print(sol.sortedArrayToBST([1]))                 # One element
+print(sol.sortedArrayToBST([]))                  # Empty list (edge case)
+print(sol.sortedArrayToBST([1,2,3,4,5,6,7]))      # Perfectly balanced possible
+print(sol.sortedArrayToBST([-10000, 0, 10000]))   # Large number boundaries
+print(sol.sortedArrayToBST(list(range(1, 10001))))# Very large input for stress test
