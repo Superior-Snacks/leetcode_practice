ac44c93956253a5bb3cf6061d89726a423b51c7e
Superior-Snacks <joningiwork@gmail.com>
2025-07-30 19:47:32 +0000

    s

--- leet_108_convert_arr_to_tree.py
+++ leet_108_convert_arr_to_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -23,7 +25,25 @@ class Solution(object):
         right = reversed(nums[mid+1:])
         print(root)
         print(left, right)
-        for i in left
+        def build_tree(arr):
+        if not arr:
+            return None
+        root = TreeNode(arr[0])
+        queue = deque([root])
+        i = 1
+        while queue and i < len(arr):
+            node = queue.popleft()
+            if node:
+                if i < len(arr):
+                    left_val = arr[i]
+                    node.left = TreeNode(left_val) if left_val is not None else None
+                    queue.append(node.left)
+                    i += 1
+                if i < len(arr):
+                    right_val = arr[i]
+                    node.right = TreeNode(right_val) if right_val is not None else None
+                    queue.append(node.right)
+                    i += 1
 
 
 """
