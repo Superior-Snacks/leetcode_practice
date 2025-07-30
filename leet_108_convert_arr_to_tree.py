a5e58ff52a5ce57d8684b473eea9ba51828ff37e
Superior-Snacks <joningiwork@gmail.com>
2025-07-30 15:13:30 +0000

    s

--- leet_108_convert_arr_to_tree.py
+++ leet_108_convert_arr_to_tree.py
@@ -16,7 +16,6 @@ Input: nums = [1,3]
 Output: [3,1]
 Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
 """
-
 sol = Solution()
 print(sol.sortedArrayToBST([-10,-3,0,5,9]))       # Mid is 0, expected root = 0
 print(sol.sortedArrayToBST([-11,-8,0,1,10,20]))
