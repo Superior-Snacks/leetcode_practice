d65889b625fbfd184300107408104ba73b073d42
Superior-Snacks <joningiwork@gmail.com>
2025-07-30 18:36:28 +0000

    s

--- leet_108_convert_arr_to_tree.py
+++ leet_108_convert_arr_to_tree.py
@@ -13,8 +13,7 @@ class Solution(object):
         else:
             mid = int(mid)
         #now add to tree
-        for i, j in nums[:mid], nums[mid:]:
-            print(i, j)
+        tree = TreeNode()
 
 """
 Given an integer array nums where the elements are sorted in ascending order, 
