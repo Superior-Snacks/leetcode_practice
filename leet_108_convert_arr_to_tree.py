07ce0538ac13d69a82abf400bc9b3f88adeb0208
Superior-Snacks <joningiwork@gmail.com>
2025-07-30 15:12:12 +0000

    s

--- leet_108_convert_arr_to_tree.py
+++ leet_108_convert_arr_to_tree.py
@@ -2,6 +2,25 @@ class Solution(object):
     def sortedArrayToBST(self, nums):
         print(nums)
 
+"""
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+"""
 
 sol = Solution()
 print(sol.sortedArrayToBST([-10,-3,0,5,9]))       # Mid is 0, expected root = 0
