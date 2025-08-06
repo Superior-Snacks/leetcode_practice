0a324e3a575c5f4051a60973da15a80e379b5ccc
Superior-Snacks <joningiwork@gmail.com>
2025-08-06 18:00:01 +0000

    s

--- leet_110_balanced_bi_tree.py
+++ leet_110_balanced_bi_tree.py
@@ -55,27 +55,19 @@ print("Test 5:", sol.isBalanced(build_tree([1, 2, None, 3])), "Expected: False")
 print("Test 6:", sol.isBalanced(build_tree([1, 2, 3, 4, 5, 6, 7])), "Expected: True")
 """
 Given a binary tree, determine if it is height-balanced.
-
- 
-
 Example 1:
-
-
 Input: root = [3,9,20,null,null,15,7]
 Output: true
+
 Example 2:
-
-
 Input: root = [1,2,2,3,3,null,null,4,4]
 Output: false
-Example 3:
 
+Example 3:
 Input: root = []
 Output: true
- 
 
 Constraints:
-
 The number of nodes in the tree is in the range [0, 5000].
 -104 <= Node.val <= 104
 """
