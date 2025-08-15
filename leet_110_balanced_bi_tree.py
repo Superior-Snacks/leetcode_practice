8882428d71fab74b741b905245ad099fd41e403e
Superior-Snacks <joningiwork@gmail.com>
2025-08-15 18:48:26 +0000

    s

--- leet_110_balanced_bi_tree.py
+++ leet_110_balanced_bi_tree.py
@@ -34,13 +34,16 @@ er það bara recursive left left þar til none svo right?
 """
 class Solution(object):
     def isBalanced(self, root):
+        return self.isBalancedcalc(root) != -1
+
+    def isBalancedcalc(self, root):
         if not root:
             return 0
-        left_height = self.isBalanced(root.left)
+        left_height = self.isBalancedcalc(root.left)
         if left_height == -1:
             return -1
         print(left_height)
-        right_height = self.isBalanced(root.right)
+        right_height = self.isBalancedcalc(root.right)
         if right_height == -1:
             return -1
         print(right_height)
