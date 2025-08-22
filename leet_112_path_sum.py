e59aa1a5495ca23caad365c38c2d9112108bef42
Superior-Snacks <joningiwork@gmail.com>
2025-08-22 19:19:53 +0000

    s

--- leet_112_path_sum.py
+++ leet_112_path_sum.py
@@ -1,9 +1,31 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
+#Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def build_tree(arr):
+    if not arr:
+        return None
+    root = TreeNode(arr[0])
+    queue = deque([root])
+    i = 1
+    while queue and i < len(arr):
+        node = queue.popleft()
+        if node:
+            if i < len(arr):
+                left_val = arr[i]
+                node.left = TreeNode(left_val) if left_val is not None else None
+                queue.append(node.left)
+                i += 1
+            if i < len(arr):
+                right_val = arr[i]
+                node.right = TreeNode(right_val) if right_val is not None else None
+                queue.append(node.right)
+                i += 1
+    return root
 class Solution(object):
     def hasPathSum(self, root, targetSum):
         ...
