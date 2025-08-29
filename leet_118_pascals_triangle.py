4940aaf8db875629da96a7f3dfcd2eb3ab71d0f2
Superior-Snacks <joningiwork@gmail.com>
2025-08-29 19:09:34 +0000

    s

--- leet_118_pascals_triangle.py
+++ leet_118_pascals_triangle.py
@@ -13,21 +13,22 @@ class Solution(object):
                 curr.append(prev[j-1] + prev[j])
             curr.append(1)
             print(curr)
+            prev = curr
 
 sol=Solution()
-print(sol.generate(1))  
+print(sol.generate(1), "done")  
 # Expected: [[1]]
 
-print(sol.generate(2))  
+print(sol.generate(2), "done")  
 # Expected: [[1], [1,1]]
 
-print(sol.generate(5))  
+print(sol.generate(5), "done")  
 # Expected: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
-print(sol.generate(6))  
+print(sol.generate(6), "done")  
 # Expected: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1],[1,5,10,10,5,1]]
 
-print(sol.generate(10))  
+print(sol.generate(10), "done")  
 # Expected last row = [1,9,36,84,126,126,84,36,9,1]
 """
 Given an integer numRows, return the first numRows of Pascal's triangle.
