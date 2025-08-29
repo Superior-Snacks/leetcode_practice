070af8bfdbfe503d3ee219be379fd5da8cc10224
Superior-Snacks <joningiwork@gmail.com>
2025-08-29 19:47:48 +0000

    s

--- leet_118.5_pascals_triangle_recursive.py
+++ leet_118.5_pascals_triangle_recursive.py
@@ -8,9 +8,9 @@ class Solution(object):
 
         # build the new row
         new_row = [1]
-        for j in range(1, len(prev_row) - 1):
-            new_row.append(prev_row[j-1] + prev_row[j])
-            new_row.append(1)
+        for j in range(1, len(prev_row)):
+            (new_row.append(prev_row[j-1] + prev_row[j]))
+        new_row.append(1)
 
         # add it to the triangle
         triangle.append(new_row)
