06d9df4b2a26e99b64c1d684c71767f8a6e05585
Superior-Snacks <joningiwork@gmail.com>
2025-09-01 11:46:15 +0000

    s

--- leet_125_valid_palindrome.py
+++ leet_125_valid_palindrome.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def isPalindrome(self, s):
-        for i in range(len(s)):
+        for i in range(len(s) // 2):
             if s[i] != s[-i]:
                 return False
         
