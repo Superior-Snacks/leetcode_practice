d514fce361098e76e959cef38c1729406882ec59
Superior-Snacks <joningiwork@gmail.com>
2025-09-01 11:47:34 +0000

    s

--- leet_125_valid_palindrome.py
+++ leet_125_valid_palindrome.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def isPalindrome(self, s):
         for i in range(len(s) // 2):
-            print(s[i])
-            print(s[-i])
+            print(f"front{s[i]}")
+            print(f"back{s[-i]}")
             if s[i] != s[-i]:
                 return False
         
