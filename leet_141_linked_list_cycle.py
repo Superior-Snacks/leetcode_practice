275e7d563f2353981da45253a7d9f5e996dbd70d
Superior-Snacks <joningiwork@gmail.com>
2025-09-02 21:03:34 +0000

    s

--- leet_141_linked_list_cycle.py
+++ leet_141_linked_list_cycle.py
@@ -39,7 +39,6 @@ head = build_linked_list([1], pos=0)
 print(sol.hasCycle(head), "expected: True")
 """
 Given head, the head of a linked list, determine if the linked list has a cycle in it.
-
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
 Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
