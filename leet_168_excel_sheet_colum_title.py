73983b4197108351da1c62a9876e55d1270a298e
Superior-Snacks <joningiwork@gmail.com>
2025-09-07 20:31:57 +0000

    s

--- leet_168_excel_sheet_colum_title.py
+++ leet_168_excel_sheet_colum_title.py
@@ -16,10 +16,10 @@ print(sol.convertToTitle(701), "expected: ZY")
 print(sol.convertToTitle(702), "expected: ZZ")
 print(sol.convertToTitle(703), "expected: AAA")
 """
-Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+Given an integer columnNumber, 
+return its corresponding column title as it appears in an Excel sheet.
 
 For example:
-
 A -> 1
 B -> 2
 C -> 3
@@ -28,23 +28,19 @@ Z -> 26
 AA -> 27
 AB -> 28 
 ...
- 
 
 Example 1:
-
 Input: columnNumber = 1
 Output: "A"
-Example 2:
 
+Example 2:
 Input: columnNumber = 28
 Output: "AB"
-Example 3:
 
+Example 3:
 Input: columnNumber = 701
 Output: "ZY"
  
-
 Constraints:
-
 1 <= columnNumber <= 231 - 1
 """
