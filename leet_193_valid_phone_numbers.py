6da1bbae11cc1e3f52c9998f477ff016f7febed6
Superior-Snacks <joningiwork@gmail.com>
2025-09-21 18:35:09 +0000

    s

--- leet_193_valid_phone_numbers.py
+++ leet_193_valid_phone_numbers.py
@@ -1,5 +1,6 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
 
+#grep -E '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' file.txt
 
 """
 Given a text file file.txt that contains a list of phone numbers (one per line), write a one-liner bash script to print all valid phone numbers.
